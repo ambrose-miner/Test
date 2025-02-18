c2a084f02cdd90f95dc40d7f4726fe254b4db16d
ambrose-miner <ambroseminer@yahoo.com>
2025-02-17 16:35:56 -0800

    new push 2/17

--- ClassProject2byAmbroseV1.1/src/main/java/com/zumba/dao/EventDao.java
+++ ClassProject2byAmbroseV1.1/src/main/java/com/zumba/dao/EventDao.java
@@ -17,17 +17,16 @@ public class EventDao {
 		try {
 			Connection con = DbConnection.getDbConnection();
 			PreparedStatement pstmt = con.prepareStatement("select * from event");
+			ResultSet rs = pstmt.executeQuery();
 			
-			ResultSet rs = pstmt.executeQuery();		 
-					while(rs.next()) {
-					
+			while(rs.next()) {
 						Event currentEvent = new Event();
 						currentEvent.setEID(rs.getInt("EID"));
 						currentEvent.setDay(rs.getString("day"));
 						currentEvent.setTime(rs.getString("time"));
 						currentEvent.setLocation(rs.getString("location"));
 						listOfevent.add(currentEvent);		
-					}
+						}
 		} catch (Exception e) {
 			System.err.println(e.toString());
 		}
@@ -41,14 +40,15 @@ public class EventDao {
 			PreparedStatement pstmt = con.prepareStatement("select * from Event innerjoin MemberEvent on Event.EID = MemberEvent.EID where MID = (?)");
 			pstmt.setInt(0, MID);
 			ResultSet rs2 = pstmt.executeQuery();
-					while(rs2.next()) {
+			
+			while(rs2.next()) {
 						Event currentEvent = new Event();
 						currentEvent.setEID(rs2.getInt("EID"));
 						currentEvent.setDay(rs2.getString("day"));
 						currentEvent.setTime(rs2.getString("time"));
 						currentEvent.setLocation(rs2.getString("location"));
 						eventsForMember.add(currentEvent);		
-					}
+						}
 		} catch (Exception e) {
 			System.err.println(e.toString());
 		}
@@ -62,14 +62,14 @@ public class EventDao {
 			PreparedStatement pstmt = con.prepareStatement("select from Event where EID = (?)");
 			pstmt.setInt(0, EID);
 			ResultSet rs3 = pstmt.executeQuery();
-					while(rs3.next()) {
+			
+			while(rs3.next()) {
 						currentSpecificEvent = new Event();
 						currentSpecificEvent.setEID(rs3.getInt("EID"));
 						currentSpecificEvent.setDay(rs3.getString("day"));
 						currentSpecificEvent.setTime(rs3.getString("time"));
 						currentSpecificEvent.setLocation(rs3.getString("location"));
-					}
-			
+						}
 		} catch (Exception e) {
 			System.err.println(e.toString());
 		}
